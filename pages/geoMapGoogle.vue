513798049b228606225cd58065b680baa2fc9209
Yaroslav-Pa <yaroslav.roro@gmail.com>
2023-05-19 20:13:42 +0300

    bubble сhart

--- pages/geoMapGoogle.vue
+++ pages/geoMapGoogle.vue
@@ -113,10 +113,46 @@ export default {
 
         backgroundColor: "transparent",
         legend: {
-          textStyle: { color: "#ffffff", fontSize: 18, "stroke-width": 0 },
+          textStyle: { color: "#ffffff", fontSize: 18 },
         },
         datalessRegionColor: "#696969",
         colorAxis: { colors: ["#95CD41", "#A3F3EB", "#FFC60B"] },
+      },
+      bubbleChartData: [
+        ["ID", "Life Expectancy", "Fertility Rate", "Region", "Population"],
+        ["CAN", 80.66, 1.67, "North America", 33739900],
+        ["DEU", 79.84, 1.36, "Europe", 81902307],
+        ["DNK", 78.6, 1.84, "Europe", 5523095],
+        ["EGY", 72.73, 2.78, "Middle East", 79716203],
+        ["GBR", 80.05, 2, "Europe", 61801570],
+        ["IRN", 72.49, 1.7, "Middle East", 73137148],
+        ["IRQ", 68.09, 4.77, "Middle East", 31090763],
+        ["ISR", 81.55, 2.96, "Middle East", 7485600],
+        ["RUS", 68.6, 1.54, "Europe", 141850000],
+        ["USA", 78.09, 2.05, "North America", 307007000],
+      ],
+      bubbleChartOptions: {
+        backgroundColor: "transparent",
+        hAxis: {
+          title: "Life Expectancy",
+          textStyle: { color: "#ffffff" },
+          titleTextStyle: {
+            color: "#ffffff",
+          },
+        },
+        vAxis: {
+          title: "Fertility Rate",
+          textStyle: { color: "#ffffff" },
+          titleTextStyle: {
+            color: "#ffffff",
+          },
+        },
+        bubble: { textStyle: { fontSize: 11 } },
+        legend: {
+          textStyle: { color: "#ffffff", fontSize: 16 },
+        },
+        width: 800,
+        height: 600,
       },
     };
   },
@@ -150,6 +186,22 @@ export default {
         type="PieChart"
         :data="pieChartData"
         :options="pieChartOptions"
+        :settings="{
+          packages: ['corechart'],
+        }"
+      />
+    </div>
+    <div
+      class="flex flex-col px-[50px] py-[30px] rounded-[50px] bg-bgColor justify-center items-center gap-y-[20px] border-2 border-secondColor"
+    >
+      <h1 class="text-center text-[20px] font-bold text-white">
+        BubbleChart: Correlation between life expectancy, fertility rate and
+        population of some world countries
+      </h1>
+      <GChart
+        type="BubbleChart"
+        :data="bubbleChartData"
+        :options="bubbleChartOptions"
         :settings="{
           packages: ['corechart'],
         }"
